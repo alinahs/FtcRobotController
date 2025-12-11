038486a36581a4d0540011af3649a0017b832602
ahsj5 <ahsj5@schls.albco>
2025-12-11 16:44:38 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecemberTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="DecemberTest", group="Linear OpMode")
+public class DecemberTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        waitForStart();
+        while(opModeIsActive()){
+            if(gamepad1.a){LFMotor.setPower(1);}
+            if(gamepad1.b){LFMotor.setPower(-1);}
+            if(gamepad1.x){LFMotor.setPower(0);}
+        }
+    }
+}
